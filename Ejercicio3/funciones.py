fc274a3944c0973283667fb57adb5c966aed6ca0
PAGF188 <pablo.garcia.fernandez.at@gmail.com>
2021-11-17 13:31:37 +0100

    arhi

--- Ejercicio3/funciones.py
+++ Ejercicio3/funciones.py
@@ -1,3 +1,38 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import pdb
+from scipy import interpolate
+import matplotlib.pyplot as plt
+
+archivos = ["./coefs/*"]
+
+def read_file(energy_valor, path):
+
+    d = {}
+    with open(path, "r") as file:
+        lines = file.readlines()[2:]
+        for line in lines:
+            aux = line.split("\t")
+            d[float(aux[0])] = float(aux[1].split("\n")[0])
+            
+    if energy_valor in d.keys():
+        return energy_valor * np.exp(-d[energy_valor])
+    else:
+        data = np.array(list(d.items()))
+        aux1 = data[:,0]
+        print(aux1.shape)
+        aux2 = data[:,1]
+        print(aux2.shape)
+        
+        f = interpolate.interp1d(data[:,0], data[:,1])
+        ynew = f(energy_valor)   # use interpolation function returned by `interp1d`
+        print(ynew)
+
+        xnew = np.arange(1, 20000, 1)
+        plt.plot(aux1, aux2, 'o', xnew, f(xnew), '-')
+        plt.show()
+
+
 def cube_phantom(size,energy):
     return None
 
@@ -7,3 +42,5 @@ def breast_phantom(size,energy):
 def vascular_phantom(size,energy):
     return None
 
+a = read_file(175,'./coefs/coefAtenuacionAdipose.csv')
+print(a)
