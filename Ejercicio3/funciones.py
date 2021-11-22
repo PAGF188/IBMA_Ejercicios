48cfc4da0eb2b932c5bd7eb91ff427ccbead12bb
PAGF188 <pablo.garcia.fernandez.at@gmail.com>
2021-11-22 12:40:20 +0100

    correcion blood file format

--- Ejercicio3/funciones.py
+++ Ejercicio3/funciones.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LinearRegression
 import pdb
 from scipy import interpolate
 import matplotlib.pyplot as plt
+import os
 
 archivos = ["./coefs/*"]
 
@@ -13,6 +14,9 @@ def read_file(energy_valor, path):
         lines = file.readlines()[2:]
         for line in lines:
             aux = line.split("\t")
+            # el archivo de blood tiene otro formato (1 celda separada por espacios)
+            if os.path.basename(path) == "coefAtenuacionBlood.csv":
+                aux = line.split(" ")
             d[float(aux[0])] = float(aux[1].split("\n")[0])
             
     if energy_valor in d.keys():
@@ -29,9 +33,18 @@ def breast_phantom(size,energy):
     return None
 
 def vascular_phantom(size,energy):
+    coef_soft = read_file(energy,'./coefs/coefAtenuacionSoft.csv')
+    coef_blood = read_file(energy,'./coefs/coefAtenuacionBlood.csv')
+
+    phantom = np.full((size,size,size),coef_soft)
+    
     return None
 
 print(read_file(40,'./coefs/coefAtenuacionAir.csv'))
 print(read_file(40,'./coefs/coefAtenuacionAdipose.csv'))
 print(read_file(40,'./coefs/coefAtenuacionBreast.csv'))
 print(read_file(40,'./coefs/coefAtenuacionSoft.csv'))
+print(read_file(40,'./coefs/coefAtenuacionBlood.csv'))
+
+
+vascular_phantom(256,40)
