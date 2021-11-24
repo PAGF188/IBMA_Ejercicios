a1f225398e6bc2be07152abfa7d00ac4bbf935ad
PAGF188 <pablo.garcia.fernandez.at@gmail.com>
2021-11-24 13:12:17 +0100

    1,2,3

--- Ejercicio4/funciones.py
+++ Ejercicio4/funciones.py
@@ -1,16 +1,38 @@
 import numpy as np
+import pdb
+import matplotlib.pyplot as plt
+
 
 def createNoiseImageN(N0, n, cellSize):
-    return None
+    # Assuming standard deviation equal do poisson distribution.
+    size = int(np.sqrt(n))
+    image = np.random.normal(N0*cellSize**2,np.sqrt(N0*cellSize**2),size=(size,size))
+    return image
 
 def createNoiseImageP(N0, n, cellSize):
-    return None
+    size = int(np.sqrt(n))
+    image = np.random.poisson(lam=N0*cellSize**2, size=(size,size))
+    return image
 
 def insertNodule(img, noduleSize, noduleContrast, N0, cellSize):
     return None
 
 def plotMiddleLine(img, N0, cellSize):
-    return None
+    # The interval limits at 2*sigma where included in the plot
+    sigma = np.std(img)
+    mean = np.mean(img)
+    filas, col = img.shape
+    valores = img[filas//2,:]
+    plt.plot(valores)
+    plt.axhline(mean+2*sigma, color='black', ls='--')
+    plt.axhline(mean-2*sigma, color='black', ls='--')
+    plt.show()
 
 def plotCellDistribution(img, numberOfBins):
+    plt.hist(img,numberOfBins, density=True, facecolor='b')
     return None
+
+N0= 4000000
+n= 100*100   # Suppose a square shape detector
+cellSize= 0.1
+imgDataP= createNoiseImageP(N0, n, cellSize)
