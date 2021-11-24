48c7ef95ee206a9b1bf51fadb965a803c7a67d0d
PAGF188 <pablo.garcia.fernandez.at@gmail.com>
2021-11-24 11:40:35 +0100

    asd

---
+++ Ejercicio4/funciones.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def createNoiseImageN(N0, n, cellSize):
+    return None
+
+def createNoiseImageP(N0, n, cellSize):
+    return None
+
+def insertNodule(img, noduleSize, noduleContrast, N0, cellSize):
+    return None
+
+def plotMiddleLine(img, N0, cellSize):
+    return None
+
+def plotCellDistribution(img, numberOfBins):
+    return None
