753346476f41cb78135b0e19e1da0287d9297e1b
PAGF188 <pablo.garcia.fernandez.at@gmail.com>
2021-11-30 12:57:16 +0100

    finalizado

--- Ejercicio4/funciones.py
+++ Ejercicio4/funciones.py
@@ -1,8 +1,5 @@
 import numpy as np
-import pdb
 import matplotlib.pyplot as plt
-from numpy.core.numeric import outer
-
 
 def createNoiseImageN(N0, n, cellSize):
     # Assuming standard deviation equal do poisson distribution.
@@ -17,17 +14,19 @@ def createNoiseImageP(N0, n, cellSize):
 
 def insertNodule(img, noduleSize, noduleContrast, N0, cellSize):
     output = img*1
-    phtons_to_each = N0*cellSize**2
-    photons_nodule = phtons_to_each * noduleContrast/100
+    phtons_total = N0*cellSize**2
+    photons_nodule = phtons_total * noduleContrast/100
+    
+    size_celdas = np.ceil(noduleSize/cellSize)  # nº of cells occupied by the nodule
 
-    # Posición
+    # Position
     sy,sx=img.shape
     x = np.linspace(-sx/2, sx/2,sx)
     y = np.linspace(-sy/2, sy/2,sy)
     x, y = np.meshgrid(x, y) 
     d = np.sqrt(x**2 + y**2)
     
-    output[d<=noduleSize/2] = img[d<=noduleSize/2] - photons_nodule
+    output[d<=np.ceil(size_celdas/2)] = img[d<=np.ceil(size_celdas/2)] - photons_nodule
     return output
 
 def plotMiddleLine(img, N0, cellSize):
@@ -41,27 +40,9 @@ def plotMiddleLine(img, N0, cellSize):
     plt.axhline(mean-2*sigma, color='black', ls='--')
     plt.axhline(mean, color='black', ls='-')
 
-    plt.show()
-
 def plotCellDistribution(img, numberOfBins):
     hist, bins= np.histogram(img, density=False, bins = numberOfBins)
     plt.plot(bins, np.insert(hist, 0, hist[0]), '-', drawstyle='steps',linewidth=1)
     plt.xlabel("No. photons")
     plt.ylabel("No. cells")
     return None
-
-N0= 4000000
-n= 100*100   # Suppose a square shape detector
-cellSize= 0.1
-imgDataP= createNoiseImageP(N0, n, cellSize)
-
-# noduleSize= 10
-# noduleContrast= 1
-# imgNodule_2= insertNodule(imgDataP, noduleSize, noduleContrast, N0, cellSize)
-
-# plt.subplot(121)
-# plt.imshow(imgDataP, cmap="gray")
-
-# plt.subplot(122)
-# plt.imshow(imgNodule_2, cmap="gray")
-# plt.show()
